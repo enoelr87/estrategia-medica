0919d58a6b93fbec90a2c4af70bceaf63fea1e02
Enoel Ramirez Rodriguez <enoelr87@gmail.com>
2024-10-14 21:29:45 -0300

    last changes

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-profile',
 	templateUrl: './profile.component.html',
-	styleUrl: './profile.component.css',
+	styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {}
--- src/app/modules/under-construction/under-construction.component.ts
+++ src/app/modules/under-construction/under-construction.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-under-construction',
 	templateUrl: './under-construction.component.html',
-	styleUrl: './under-construction.component.css',
+	styleUrl: './under-construction.component.scss',
 })
 export class UnderConstructionComponent {}
